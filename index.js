b6b9fb569d4ed1fb5a9c02ad5cf5b8c6ea29d889
18OJohnston.acc@lwetb.ie <18OJohnston.acc@lwetb.ie>
2022-09-28 10:37:23 +0100

    Finished!

--- index.js
+++ index.js
@@ -13,9 +13,49 @@ if (first_number > second_number){
 	alert(String(first_number) + " is equal too " + String(second_number))
 }
 
-let numbers = [for (i of ["1","2","3"]) Number(prompt("Number " + i + ":"))];
+// Program 3:
+let numbers = []
+for (let i = 1; i <= 4; i++) {
+	numbers.push(Number(prompt("Number "+i+":")));
+}
+
 numbers.sort();
 numbers.reverse();
 
-alert("The largest number is: " + String(numbers[0]))
+alert("The largest number is: " + String(numbers[0]));
+
+
+// Program 4:
+let sides = [];
+for (i=1; i<=3; i++){
+	sides.push(Number(prompt("Side "+i+":")));
+}
+
+if (sides[0] == sides[1] && sides[0] == sides[2]) {
+	alert("Equilateral")
+} else if (sides[0] == sides[1] || sides[1] == sides[2] || sides[2] == sides[0]) {
+	alert("Isosceles")
+} else {
+	alert("Scalene")
+}
+
+
+// Program 5:
+let operations = {
+	"add": (a, b) => {return a+b;},
+	"subtract": (a, b) => {return a-b;},
+	"multiply": (a, b) => {return a*b},
+	"divide": (a,b) => {return a/b},
+	"modulus": (a,b) => {return a%b}
+}
+
+let num_1 = Number(prompt("num 1:"));
+let num_2 = Number(prompt("num 2:"));
+let operator = prompt("operator:");
+
+if (operator in operations) {
+	alert("The answer is: " + operations[operator](num_1, num_2))
+} else {
+	alert("Invalid operation")
+}
 
